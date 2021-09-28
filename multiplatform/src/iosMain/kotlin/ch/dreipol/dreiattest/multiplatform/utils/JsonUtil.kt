1f78d03afddf73d610b9345ad769d0821fbd2233
Nils Becker <nils.becker@dreipol.ch>
2021-09-28 10:48:27 +0200

    [release_prep] Correct json encoding on iOS

--- multiplatform/src/iosMain/kotlin/ch/dreipol/dreiattest/multiplatform/utils/JsonUtil.kt
+++ multiplatform/src/iosMain/kotlin/ch/dreipol/dreiattest/multiplatform/utils/JsonUtil.kt
@@ -8,7 +8,7 @@ import platform.Foundation.NSString
 actual object JsonUtil {
     @ExperimentalUnsignedTypes
     actual fun sortedJsonData(value: Map<String, String>): ByteArray {
-        val data = NSJSONSerialization.dataWithJSONObject(value, NSJSONWritingPrettyPrinted.or(NSJSONWritingSortedKeys), null)
+        val data = NSJSONSerialization.dataWithJSONObject(value, NSJSONWritingSortedKeys, null)
         return data?.let { Conversion.dataToByteArray(it) } ?: ByteArray(0)
     }
 }
