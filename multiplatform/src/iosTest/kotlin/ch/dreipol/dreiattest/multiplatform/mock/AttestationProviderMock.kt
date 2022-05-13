487bf3de71f958cd3a0df880312794cd439100b0
Kai Widmer <kaiwi@bluewin.ch>
2022-05-13 08:59:35 +0200

    [1.6] Fix iOS test execution

--- multiplatform/src/iosTest/kotlin/ch/dreipol/dreiattest/multiplatform/mock/AttestationProviderMock.kt
+++ multiplatform/src/iosTest/kotlin/ch/dreipol/dreiattest/multiplatform/mock/AttestationProviderMock.kt
@@ -3,15 +3,18 @@ package ch.dreipol.dreiattest.multiplatform.mock
 import ch.dreipol.dreiattest.multiplatform.AttestationProvider
 import ch.dreipol.dreiattest.multiplatform.api.Attestation
 import ch.dreipol.dreiattest.multiplatform.platformDriver
-import ch.dreipol.dreiattest.multiplatform.utils.CryptoUtils
-import ch.dreipol.dreiattest.multiplatform.utils.Hash
-import ch.dreipol.dreiattest.multiplatform.utils.encodeHashedToBase64
-import ch.dreipol.dreiattest.multiplatform.utils.encodeToBase64
+import ch.dreipol.dreiattest.multiplatform.utils.*
+import io.ktor.utils.io.core.*
 
 actual class AttestationProviderMock : AttestationProvider {
-    override suspend fun getAttestation(nonce: Hash, publicKey: ByteArray): Attestation {
+    override val systemInfo = SystemInfoMock
+    override val isSupported: Boolean
+        get() = true
+
+    override suspend fun getAttestation(nonce: Hash, publicKey: String): Attestation {
         return Attestation(
-            keyId = CryptoUtils.encodeToBase64(publicKey), attestation = CryptoUtils.encodeHashedToBase64(nonce),
+            publicKey = CryptoUtils.encodeToBase64(publicKey.toByteArray()),
+            attestation = CryptoUtils.encodeHashedToBase64(nonce),
             driver = platformDriver
         )
     }
