387a2c2a117b1d11d6b37a8e68fce61cd6978a95
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2025-09-11 09:04:14 +0200

    [update-dependencies] Remove !!

--- multiplatform/src/androidMain/kotlin/ch/dreipol/dreiattest/multiplatform/utils/DeviceSystemInfo.kt
+++ multiplatform/src/androidMain/kotlin/ch/dreipol/dreiattest/multiplatform/utils/DeviceSystemInfo.kt
@@ -5,8 +5,8 @@ import android.content.Context
 internal class DeviceSystemInfo(context: Context) : SystemInfo {
     private val packageInfo = context.packageManager.getPackageInfo(context.packageName, 0)
 
-    override val appVersion: String
-        get() = packageInfo.versionName!!
+    override val appVersion: String?
+        get() = packageInfo.versionName
 
     override val appBuild: String
         get() = packageInfo.versionCode.toString()
